51c501fee2d98fc63caa5794ba7cb0be9c54826c
Olumide Bambe <loyalsfc@gmail.com>
2022-09-05 19:41:53 +0100

    Adding leagues standing

--- src/pages/Standing.js
+++ src/pages/Standing.js
@@ -5,12 +5,14 @@ function Standing(){
     const [stand, setStand] = useState([]);
     const [leagues, setLeagues] = useState([]);
     const [leaguesSearch, setleaguesSearch] = useState('');
+    const [searches, setSearches] = useState('');
+    const [leagueId, setLeagueId] =  useState (152)
 
     useEffect(()=>{
-        fetch(`https://apiv3.apifootball.com/?action=get_standings&league_id=302&APIkey=${key}`)
+        fetch(`https://apiv3.apifootball.com/?action=get_standings&league_id=${leagueId}&APIkey=${key}`)
         .then((res, req) => res.json())
         .then(data => setStand(data))
-    },[])
+    },[leagueId])
 
     useEffect(()=>{
         fetch(`https://apiv3.apifootball.com/?action=get_leagues&country_id=&APIkey=${key}`)
@@ -23,23 +25,35 @@ function Standing(){
             <tr key={item.overall_league_position}>
                 <td className="table-team teams">{item.team_name}</td>
                 <td>{item.overall_league_payed}</td>
-                <td>{item.overall_league_W}</td>
-                <td>{item.overall_league_D}</td>
-                <td>{item.overall_league_L}</td>
-                <td>{item.overall_league_GF}</td>
-                <td>{item.overall_league_GA}</td>
+                <td className="d-none">{item.overall_league_W}</td>
+                <td className="d-none">{item.overall_league_D}</td>
+                <td className="d-none">{item.overall_league_L}</td>
+                <td className="d-none">{item.overall_league_GF}</td>
+                <td className="d-none">{item.overall_league_GA}</td>
                 <td>{item.overall_league_GF - item.overall_league_GA}</td>
                 <td>{item.overall_league_PTS}</td>
             </tr>
         )
     })
 
+    function getLeagueId(event){
+        setLeagueId(event.target.getAttribute("data-league-id"))
+    }
+
     function handleChange(event){
         setleaguesSearch(event.target.value)
-
-        const searchedResult = leagues.filter(league => league.league_name.includes(event.target.value))
-
-        console.log(searchedResult)
+        const searchedResult = leagues.filter(league => league.league_name.toLowerCase().includes(event.target.value.toLowerCase()))
+        setSearches(searchedResult.map(result => {
+            if(event.target.value != ""){
+                return <div 
+                            key={result.league_id} 
+                            data-league-id={result.league_id}
+                            onClick={getLeagueId}
+                        >
+                            {result.country_name + " " + result.league_name}
+                        </div>
+            }
+        }))
     }
 
     return(
@@ -53,27 +67,33 @@ function Standing(){
                     </>
                 }
             </ul>
-            <div className="search-wrapper">
-                <input 
-                    type="text" 
-                    id="search-league" 
-                    name="search-league" 
-                    className="search-league" 
-                    onChange={handleChange}
-                    value={leaguesSearch}
-                />
-                <i className="fa-solid fa-magnifying-glass"></i>
+            <div className="search-container">
+                <div className="search-wrapper">
+                    <input 
+                        type="text" 
+                        id="search-league" 
+                        name="search-league" 
+                        className="search-league" 
+                        onChange={handleChange}
+                        onBlur={()=>{setleaguesSearch("")}}
+                        value={leaguesSearch}
+                    />
+                    <i className="fa-solid fa-magnifying-glass"></i>
+                </div>
+                <div className="search-result">
+                    {searches}
+                </div>
             </div>
             <table>
                 <thead>
                     <tr>
                         <th className="table-team">Team</th>
                         <th>Played</th>
-                        <th>Won</th>
-                        <th>Drawn</th>
-                        <th>Lost</th>
-                        <th>+</th>
-                        <th>-</th>
+                        <th className="d-none">Won</th>
+                        <th className="d-none">Drawn</th>
+                        <th className="d-none">Lost</th>
+                        <th className="d-none">+</th>
+                        <th className="d-none">-</th>
                         <th>GD</th>
                         <th>Points</th>
                     </tr>
